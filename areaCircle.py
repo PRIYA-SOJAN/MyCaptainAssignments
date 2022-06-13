575e8727c4b49a49badff8f7197a2e9182bbcccf
PRIYA-SOJAN <107124862+PRIYA-SOJAN@users.noreply.github.com>
2022-06-13 16:28:46 +0530

    Add files via upload
    Task 1 : Computing area of circle with radius as 1.1

---
+++ areaCircle.py
@@ -0,0 +1,9 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+import math
+print(math.pi)
+3.141592653589793
+radius=1.1
+area=radius*radius
+print("area of the circle with radius 1.1 is",(math.pi*area))
+area of the circle with radius 1.1 is 3.8013271108436504
