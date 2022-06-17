638652c7cb579c2bcf7aeebd3619c53f32355f41
PRIYA-SOJAN <107124862+PRIYA-SOJAN@users.noreply.github.com>
2022-06-17 11:05:29 +0530

    Add files via upload

---
+++ abc.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 14 09:34:16 2022
+
+@author: dell
+"""
+
+import math
+print(math.pi)
+radius=1.1
+area=math.pi*radius*radius
+print("area of the circle with radius 1.1 is",(area))
