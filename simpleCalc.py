cb8faa3613847b0dde22a34ead0ecf235baf4348
PRIYA-SOJAN <107124862+PRIYA-SOJAN@users.noreply.github.com>
2022-06-19 17:44:35 +0530

    Add files via upload

---
+++ simpleCalc.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 19 11:32:43 2022
+
+@author: dell
+"""
+
+x=1
+y=2
+operation=not sum
+print("Result is")
+if operation==sum:
+    print(x+y)
+else:
+    print(x-y)
+    
+    
